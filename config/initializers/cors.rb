9ecc19147acdc22b5bcde605a3797c0b9027a6d0
kenta-s <knt01222@gmail.com>
2019-11-16 18:26:55 +0900

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,18 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+
+    if Rails.env.development?
+      origins 'localhost:5000'
+    elsif Rails.env.production?
+      origins 'example.com'
+    end
+
+    resource '*',
+      headers: :any,
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
